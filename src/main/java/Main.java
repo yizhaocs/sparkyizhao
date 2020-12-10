9b697668409e43ac0804a7502609ba666f028283
YI ZHAO <yizhao@yis-macbook-pro.lan>
2020-12-09 20:36:06 -0800

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.google.inject.internal.cglib.proxy.$FixedValue;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -7,10 +8,40 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
+        // loadCollectionIntoRDDExample();
         // reduceExample();
-        mapExample();
+        // mapExample();
+        printEachItemInRDDExample();
+        // countHowManyItemsInRDDExample();
     }
 
+
+    /*
+        using JavaSparkContext's parallelize method to load collection into RDD
+     */
+    public static void loadCollectionIntoRDDExample(){
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(35.5);
+        inputData.add(12.49943);
+        inputData.add(90.32);
+        inputData.add(20.32);
+
+        /*
+            local uses 1 thread only.
+            local[n] uses n threads.
+            local[*] uses as many threads as your spark local machine have, where you are running your application.
+         */
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+
+        sc.close();
+    }
+
+    /*
+        reduce means reduce the collection into the single answer
+     */
     public static void reduceExample(){
         List<Double> inputData = new ArrayList<>();
         inputData.add(35.5);
@@ -34,6 +65,9 @@ public class Main {
         sc.close();
     }
 
+    /*
+        The map method on an RDD allows us to transform the structure of RRD from one to another.
+     */
     public static void mapExample(){
         List<Integer> inputData = new ArrayList<>();
         inputData.add(35);
@@ -57,6 +91,68 @@ public class Main {
 
         sqrtRdd.collect().forEach(System.out::println);
         System.out.println("result:" + result); // result:157
+
+        sc.close();
+    }
+
+    /*
+        print Each Item In RDD
+    */
+    public static void printEachItemInRDDExample(){
+        List<Integer> inputData = new ArrayList<>();
+        inputData.add(35);
+        inputData.add(12);
+        inputData.add(90);
+        inputData.add(20);
+
+        /*
+            local uses 1 thread only.
+            local[n] uses n threads.
+            local[*] uses as many threads as your spark local machine have, where you are running your application.
+         */
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
+
+        /*
+            35
+            12
+            90
+            20
+         */
+        myRdd.collect().forEach(System.out::println);
+
+        sc.close();
+    }
+
+    /*
+        count How Many Items In RDD
+     */
+    public static void countHowManyItemsInRDDExample(){
+        List<Integer> inputData = new ArrayList<>();
+        inputData.add(35);
+        inputData.add(12);
+        inputData.add(90);
+        inputData.add(20);
+
+        /*
+            local uses 1 thread only.
+            local[n] uses n threads.
+            local[*] uses as many threads as your spark local machine have, where you are running your application.
+         */
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
+
+        /*
+            We use the map process but we just simply map every single value in RDD to the output value of 1 and then we do reduce,
+            and reduce can simply add up all of the values in the second RDD.
+         */
+        JavaRDD<Integer> singleIntegerRdd = myRdd.map(value -> 1);
+        Integer count = singleIntegerRdd.reduce((value1, value2) -> value1 + value2);
+        System.out.println("count:" + count); // 4
 
         sc.close();
     }
